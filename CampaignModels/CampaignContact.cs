fadcd301d317f19aec5b0f27cfc49b7618e5e901
loublick <loublick@microsoft.com>
2023-05-15 15:49:17 -0400

    Update repo

--- CampaignModels/CampaignContact.cs
+++ CampaignModels/CampaignContact.cs
@@ -9,10 +9,23 @@ namespace CampaignModels
     /// Campaign contact information class used to pass campaign and member contact
     /// data to CampaignMailer Azure functions.
     /// </summary>
-    public class Contact
+    public class CampaignContact
     {
         public string? FullName { get; set; }
 
         public string? EmailAddress { get; set; }
+
+        public string? ReplyToEmailAddress { get; set; }
+
+        public string? ReplyToDisplayName { get; set; }
+
+        public string? MessageSubject { get; set; }
+
+        public string? MessageBodyHtml { get; set; }
+
+        public string? MessageBodyPlainText { get; set; }
+
+        public string? SenderEmailAddress { get; set; }
+
     }
 }
